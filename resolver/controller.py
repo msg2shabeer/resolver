b76df5dae731e6ab18272de9395ea809d682db94
Shabeer <msg2shabeer@gmail.com>
2017-02-14 21:19:38 +0530

    [todo] on controller

--- resolver/controller.py
+++ resolver/controller.py
@@ -1,4 +1,20 @@
 import resolver
+
+# Home
 @resolver.app.route('/')
 def hello_world():
 	return 'Welcome to resolver'
+
+# Get all users
+
+# Get a single user
+
+# Add a new user
+
+# Get all complaints
+
+# Get a single complaint by its ID
+
+# Get complaints by customer ID
+
+# Add a complaint
