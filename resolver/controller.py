1d03e5661f791adf62604b49bcc7facc4f6afaa5
Shabeer <msg2shabeer@gmail.com>
2017-02-15 22:18:44 +0530

    [add] new route /users/

--- resolver/controller.py
+++ resolver/controller.py
@@ -1,11 +1,15 @@
 from resolver import app
-
+from resolver.models import User
+from flask import jsonify
 # Home
 @app.route('/')
 def hello_world():
 	return 'Welcome to resolver'
 
 # Get all users
+@app.route('/users/', methods = ['GET'])
+def get_users():
+	return jsonify({'users': User.query.all()})
 
 # Get a single user
 
