23b4252ed03accb5f1e66f8f785362c69eb568ef
Shabeer M <msg2shabeer@gmail.com>
2017-02-24 12:35:42 +0530

    [edit] while updating complaint status fetching first query result instead of an array

--- resolver/controller.py
+++ resolver/controller.py
@@ -55,9 +55,8 @@ def put_complaint():
 # Change complaint status
 @app.route('/complaints/<int:id>/status/',methods = ['PUT'])
 def change_complaint_status(id):
-	complaint=Complaint.query.filter_by(id=id)
-	for c in complaint:
-		c.status_id=request.json['status_id']
-		db.session.add(c)
+	complaint=Complaint.query.filter_by(id=id).first() #return first result
+	complaint.status_id = request.json['status_id']
+	db.session.add(complaint)
 	db.session.commit()
 	return jsonify({'message' : 'Complaint Status Changed Successfully'}), 200
