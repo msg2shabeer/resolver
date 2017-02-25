d778364300d549ce625d67e08abfb907bee12351
Muhammed Shameem <mailtoshameempk@gmail.com>
2017-02-25 16:45:40 +0530

    [Add] marshmallow schemas to models

--- resolver/models.py
+++ resolver/models.py
@@ -1,9 +1,8 @@
 from resolver import db
 from datetime import datetime
-
+from marshmallow import Schema, fields, post_load
 
 class User(db.Model):
-	'''User table to store the user information, includes admin and staffs'''
 	id=db.Column(db.Integer, primary_key=True)
 	name=db.Column(db.String(25))
 	user_name=db.Column(db.String(20), unique=True)
@@ -20,8 +19,18 @@ class User(db.Model):
 	def __repr__(self):
 		return '<User %r>' % self.user_name
 
+class UserSchema(Schema):
+	id=fields.Integer()
+	name=fields.Str()
+	user_name=fields.Str()
+	password=fields.Str()
+	user_type_id=fields.Integer()
+
+	@post_load
+	def make_user(self, data):
+		return User(**data)
+
 class UserType(db.Model):
-	'''Table storing category details of users'''
 	id=db.Column(db.Integer, primary_key=True)
 	name=db.Column(db.String(25))
 
@@ -31,8 +40,15 @@ class UserType(db.Model):
 	def __repr__(self):
 		return '<User Type %r>' % self.name
 
+class UserTypeSchema(Schema):
+	id=fields.Integer()
+	name=fields.Str()
+
+	@post_load
+	def make_user_type(self, data):
+		return UserType(**data)
+
 class Complaint(db.Model):
-	'''Complaints table'''
 	id=db.Column(db.Integer, primary_key=True)
 	cust_id=db.Column(db.String(25))
 	cust_name=db.Column(db.String(30))
@@ -40,7 +56,7 @@ class Complaint(db.Model):
 	cust_phone=db.Column(db.String(15))
 	complaint_phone=db.Column(db.String(15))
 	no_calls=db.Column(db.Integer, default=1)
-	date_time=db.Column(db.DateTime)
+	date_time=db.Column(db.DateTime, default=datetime.utcnow())
 	priority=db.Column(db.Float, default=0.00)
 	
 	service_id=db.Column(db.Integer, db.ForeignKey('service.id'))
@@ -69,8 +85,25 @@ class Complaint(db.Model):
 	def __repr__(self):
 		return '<Complaint id:%r p:%r d:%r c_id:%r>' %(self.id,self.priority,self.date_time,self.cust_id)
 
+class ComplaintSchema(Schema):
+	id=fields.Integer()
+	cust_id=fields.Str()
+	cust_name=fields.Str()
+	cust_address=fields.Str()
+	cust_phone=fields.Str()
+	complaint_phone=fields.Str()
+	no_calls=fields.Integer()
+	date_time=fields.DateTime()
+	priority=fields.Float()
+	service_id=fields.Integer()
+	complaint_type_id=fields.Integer()
+	status_id=fields.Integer()
+
+	@post_load
+	def make_complaint(self, data):
+		return Complaint(**data)
+
 class Service(db.Model):
-	'''Service'''
 
 	id=db.Column(db.Integer, primary_key=True)
 	name=db.Column(db.String(20), unique=True)
@@ -78,8 +111,15 @@ class Service(db.Model):
 	def __init__(self, name):
 		self.name=name
 
+class ServiceSchema(Schema):
+	id=fields.Integer()
+	name=fields.Str()
+
+	@post_load
+	def make_service(self, data):
+		return Service(**data)
+
 class ComplaintType(db.Model):
-	'''ComplaintType'''
 	
 	id=db.Column(db.Integer, primary_key=True)
 	name=db.Column(db.String(20), unique=True)
@@ -89,12 +129,28 @@ class ComplaintType(db.Model):
 	def __init__(self, name, service_id):
 		self.name=name
 		self.service_id=service_id
+
+class ComplaintTypeSchema(Schema):
+	id=fields.Integer()
+	name=fields.Str()
+	service_id=fields.Integer()
+
+	@post_load
+	def make_complaint_type(self, data):
+		return ComplaintType(**data)
 		
 class ComplaintStatus(db.Model):
-	'''ComplaintStatus'''
 	
 	id=db.Column(db.Integer, primary_key=True)
 	name=db.Column(db.String(20), unique=True)
 
 	def __init__(self, name):
 		self.name=name
+
+class ComplaintStatusSchema(Schema):
+	id=fields.Integer()
+	name=fields.Str()
+
+	@post_load
+	def make_complaint_status(self, data):
+		return ComplaintStatus(**data)
