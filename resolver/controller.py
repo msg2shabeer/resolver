911a55a9eafafc887fb616dbf8f84307ca95f823
Muhammed Shameem <mailtoshameempk@gmail.com>
2017-02-26 00:21:09 +0530

    [Edit] makes controller to use marshmallow schemas

--- resolver/controller.py
+++ resolver/controller.py
@@ -10,20 +10,23 @@ def hello_world():
 # Get all users
 @app.route('/users/', methods = ['GET'])
 def get_users():
-	users=jsonify({'users':[UserJsonSerializer().serialize(x) for x in User.query.all()]})
-	return users, 200
+	users=User.query.all()
+	return jsonify({'users' : UserSchema().dump(users, many=True).data}), 200
 
 # Get a single user
 @app.route('/users/<int:id>', methods = ['GET'])
 def get_user(id):
-	user = jsonify({'user':[UserJsonSerializer().serialize(x) for x in User.query.filter_by(id=id)]})
-	return user, 200
+	user = User.query.filter_by(id=id).first()
+	if user:
+		return jsonify({'user': UserSchema().dump(user).data}), 200
+	else:
+		return jsonify({'message' : 'User Do not Exist with id:'+str(id)}), 500
 
 # Add a new user
 @app.route('/users/', methods = ['POST'])
 def put_user():
 	try:
-		db.session.add(UserJsonSerializer().deserialize(request.json))
+		db.session.add(UserSchema().load(request.json).data)
 		db.session.commit()
 	except Exception as e:
 		return jsonify({'message' : 'User Creation Failed'}), 500
@@ -33,26 +36,32 @@ def put_user():
 # Get all complaints
 @app.route('/complaints/', methods=['GET'])
 def get_complaints():
-	complaints=jsonify({'complaints':[ComplaintJsonSerializer(utc_offset=app.config['UTC_OFFSET']).serialize(x) for x in Complaint.query.all()]})
-	return complaints, 200
+	complaints=Complaint.query.all()
+	return jsonify({'complaints' : ComplaintSchema().dump(complaints, many=True).data}), 200
 
 # Get a single complaint by its ID
 @app.route('/complaints/<int:id>', methods = ['GET'])
 def get_complaint(id):
-	complaint = jsonify({'complaint':[ComplaintJsonSerializer(utc_offset=app.config['UTC_OFFSET']).serialize(x) for x in Complaint.query.filter_by(id=id)]})
-	return complaint, 200
+	complaint = Complaint.query.filter_by(id=id).first()
+	if complaint:
+		return jsonify({'complaint' : ComplaintSchema().dump(complaint).data}), 200
+	else:
+		return jsonify({'message' : 'Complaints Do not Exist with id:'+str(id)}), 500
 
 # Get complaints by customer ID
-@app.route('/complaints/cust/<int:id>', methods = ['GET'])
+@app.route('/complaints/cust/<string:cust_id>', methods = ['GET'])
 def get_complaint_cust(id):
-	complaint = jsonify({'complaint':[ComplaintJsonSerializer(utc_offset=app.config['UTC_OFFSET']).serialize(x) for x in Complaint.query.filter_by(id=id)]})
-	return complaint, 200
+	complaints = Complaint.query.filter_by(cust_id=str(cust_id))
+	if complaints:
+		return jsonify({'complaints' : ComplaintSchema().dump(complaints, many=True).data}), 200
+	else:
+		return jsonify({'message' : 'Complaints Do not Exist for customer id:'+str(id)}), 500
 
 # Add a complaint --- Check for existing complaint
 @app.route('/complaints/', methods = ['POST'])
 def put_complaint():
 	try:
-		db.session.add(ComplaintJsonSerializer().deserialize(request.json))
+		db.session.add(ComplaintSchema().load(request.json).data)
 		db.session.commit()
 	except Exception as e:
 		return jsonify({'message' : 'Complaint Creation Failed'}), 500
@@ -62,7 +71,7 @@ def put_complaint():
 # Change complaint status
 @app.route('/complaints/<int:id>/status/',methods = ['PUT'])
 def change_complaint_status(id):
-	complaint=Complaint.query.filter_by(id=id).first() #return first result
+	complaint=Complaint.query.filter_by(id=id).first()
 	if not complaint:
 		return jsonify({'message' : 'Complaint Status Changing failed, there is no such complaint'}), 500
 	complaint.status_id = request.json['status_id']
